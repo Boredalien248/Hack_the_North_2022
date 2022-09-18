cd104b6250c74ded72952674b487aab1293664ab
Boredalien248 <65315430+Boredalien248@users.noreply.github.com>
2022-09-18 01:59:29 -0400

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4"
+  "FLUTTER_TARGET=C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\kaust\\Hack the North\\New\\project_1\\application_v4\\.dart_tool\\package_config.json"
+)
